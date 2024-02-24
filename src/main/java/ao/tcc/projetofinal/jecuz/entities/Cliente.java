645c0c96c5dacb9a7e59a13cbc70b54cfb3a1ddd
KaundaDaniel <dannydagmaisutic@gmail.com>
2024-02-24 23:18:03 +0100

    Crud de Servico

--- src/main/java/ao/tcc/projetofinal/jecuz/entities/Cliente.java
+++ src/main/java/ao/tcc/projetofinal/jecuz/entities/Cliente.java
@@ -5,36 +5,7 @@ import lombok.*;
 
 import java.util.List;
 
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
+
 public class Cliente {
-    @Id
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name="nome_completo", nullable = false)
-    private String nomeCompleto;
-    @Column(unique = true, nullable = false)
-    private String email;
-    @Column(nullable = false)
-    private String senha;
-
-    @Column(nullable = false, unique = true, length = 14)
-    private String numeroBi;
-
-    @OneToOne(fetch = FetchType.LAZY, orphanRemoval=true)
-    @JoinColumn(name = "foto_documento", nullable = true)
-    private Foto fotoDocumento;
-
-    @ManyToMany
-    @JoinTable(
-            name = "municipio_cliente",
-            joinColumns = @JoinColumn(name = "cliente_id"),
-            inverseJoinColumns = @JoinColumn(name = "municipio_id"))
-    private List<Municipio> municipal;
 
 }

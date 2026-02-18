d32ef620141cb46ac12af74c18b50948095409bc
ivancarlosantos <ivansantos.ronem@gmail.com>
2026-02-18 19:05:42 -0300

    feat: add sonarqube token

--- sonar.sh
+++ sonar.sh
@@ -4,7 +4,7 @@ set -e
 sleep 3
 mvn clean test jacoco:report
 sleep 2
-mvn clean verify sonar:sonar -Dsonar.projectKey=jecuz -Dsonar.host.url=http://localhost:9030 -Dsonar.login=squ_2385046cb43ff4e10fbb1c3b8860ca9e55cbe878
+mvn clean verify sonar:sonar -Dsonar.projectKey=jecuz -Dsonar.host.url=http://jecuz:9030 -Dsonar.login=squ_2385046cb43ff4e10fbb1c3b8860ca9e55cbe878
 
 exit 0
 
